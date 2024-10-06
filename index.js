4d1d3d71e9a0e4e125bebbcb897d765306327bee
RonalTam <lakeheart001199@gmail.com>
2024-10-07 00:24:51 +0700

    7/10

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const cartBtn = document.getElementById("cart-btn");
-    const minicart = document.getElementById("minicart");
-    const cartItemsContainer = document.getElementById("cart-items");
-    const cartCount = document.getElementById("cart-count");
-    const cartTotal = document.getElementById("cart-total");
-    let cart = [];
-  
-    // Hiển thị/ẩn Minicart khi nhấn vào biểu tượng giỏ hàng
-    cartBtn.addEventListener("click", function() {
-      minicart.style.display = (minicart.style.display === "none" || !minicart.style.display) ? "block" : "none";
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const cartBtn = document.getElementById("cart-btn");
+  const minicart = document.getElementById("minicart");
+  const cartItemsContainer = document.getElementById("cart-items");
+  const cartCount = document.getElementById("cart-count");
+  const cartTotal = document.getElementById("cart-total");
+  let cart = [];
 
+  // Hiển thị/ẩn Minicart khi nhấn vào biểu tượng giỏ hàng
+  cartBtn.addEventListener("click", function () {
+    minicart.style.display = (minicart.style.display === "none" || !minicart.style.display) ? "block" : "none";
   });
+});
+
+
+$(document).ready(function () {
+  $('.products-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+  });
+});
